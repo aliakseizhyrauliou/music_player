8f9058740d3d3cee502544ecc7110f3bc3409a0f
aliakseizhyrauliou <aliaksei.zhurauliou@yandex.by>
2023-10-11 13:19:06 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ import { SeedModule } from 'db/seed/seed.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-
-    origin:"http://localhost:5173/",
+      origin: 'http://localhost:5173', 
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      allowedHeaders: 'Content-Type,Authorization',
     });
 
   const seedModule = app.select(SeedModule); 
